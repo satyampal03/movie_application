bc6a5a0fafa24245b5e26ad6d443f947c96b8fef
satyampal <em.satyam@gmail.com>
2025-11-17 22:57:21 +0400

    Today's final data pushed #03

---
+++ script/main.js
@@ -0,0 +1,27 @@
+const getMovie = async () => {
+    try{
+        const getFetch = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=387a089c850f9bb023b31ffad051f531`)
+
+        const data = await getFetch.json();
+
+
+        console.log(data.results[0].adult);
+        console.log(data.results[0].backdrop_path);
+        console.log(data.results[0].genre_ids);
+        console.log(data.results[0].id);
+        console.log(data.results[0].original_language);
+        console.log(data.results[0].original_title);
+        console.log(data.results[0].overview);
+        console.log(data.results[0].popularity);
+        console.log(data.results[0].poster_path);
+        console.log(data.results[0].release_date);
+        console.log(data.results[0].title);
+        console.log(data.results[0].video);
+        console.log(data.results[0].vote_average);
+        console.log(data.results[0].vote_count);
+
+    }catch(err){
+        console.log(err);
+    }
+}
+getMovie();
